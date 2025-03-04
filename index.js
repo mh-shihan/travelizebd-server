c591e1ae5a0cf77bf9e997ef08befaf7003e21f5
Mahmudul Hasan Shihan <123587075+mh-shihan@users.noreply.github.com>
2025-03-04 22:14:41 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 
-const port = process.env.POET || 5000;
+const port = process.env.PORT || 5000;
 const secret = process.env.ACCESS_TOKEN_SECRET;
 
 //  parser
